2837166236a68d456d1caf8e6acc44817775952a
Atanas Kambitov <45592155+AtanasKambitov@users.noreply.github.com>
2020-01-20 18:09:35 +0200

    pages created

---
+++ react-firebase-authentication/src/components/App/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navigation from "../Navigation/navigation";
+
+const App = () => (
+  <Router>
+    <Navigation />
+  </Router>
+);
+export default App;
